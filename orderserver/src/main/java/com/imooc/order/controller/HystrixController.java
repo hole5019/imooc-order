fbb12763b5b32f590894528159c5267348c37e78
helei <heleihg@163.com>
2018-11-28 16:33:46 +0800

    10-5 使用配置项

--- orderserver/src/main/java/com/imooc/order/controller/HystrixController.java
+++ orderserver/src/main/java/com/imooc/order/controller/HystrixController.java
@@ -5,6 +5,7 @@ import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
@@ -19,11 +20,22 @@ public class HystrixController {
 
     //@HystrixCommand(fallbackMethod = "fallback")
 //    @HystrixCommand
-    @HystrixCommand(commandProperties = {
-            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds",value = "3000")
-    })
+//    @HystrixCommand(commandProperties = {
+//            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds",value = "3000")
+//    })
+//    @HystrixCommand(commandProperties = {
+//            @HystrixProperty(name = "circuitBreaker.enabled",value = "true"),
+//            @HystrixProperty(name = "circuitBreaker.requestVolumeThreshold",value = "10"),
+//            @HystrixProperty(name = "circuitBreaker.sleepWindowInMilliseconds",value = "10000"),
+//            @HystrixProperty(name = "circuitBreaker.errorThresholdPercentage",value = "60")
+//    })
+    @HystrixCommand(commandKey = "getProductInfoList2")
     @GetMapping(value = "/getProductInfoList",produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    public String getProductInfoList(){
+    public String getProductInfoList(@RequestParam("num") Integer num){
+        if(num % 2 == 0){
+            return "success";
+        }
+
         RestTemplate restTemplate = new RestTemplate();
         return restTemplate.postForObject("http://localhost:8762/product/listForOrder",
                 Arrays.asList("157875196366160022"),
