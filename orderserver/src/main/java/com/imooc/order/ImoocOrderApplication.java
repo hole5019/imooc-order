59eabd28d422bb49be7b45d55ae72ca90609ea5b
helei <heleihg@163.com>
2018-11-28 17:24:43 +0800

    10-6 feign-hystrix的使用

--- orderserver/src/main/java/com/imooc/order/ImoocOrderApplication.java
+++ orderserver/src/main/java/com/imooc/order/ImoocOrderApplication.java
@@ -6,12 +6,14 @@ import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
 
 @EnableFeignClients(basePackages = "com.imooc.product.client")
 //@SpringBootApplication
 //@EnableDiscoveryClient
 //@EnableCircuitBreaker
 @SpringCloudApplication
+@ComponentScan(basePackages = "com.imooc")
 public class ImoocOrderApplication {
 
 	public static void main(String[] args) {
